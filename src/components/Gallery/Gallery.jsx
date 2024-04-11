52e3eb6fa4f6a48a46762d90964ef61a26a0bee2
hashimshah47 <hashimtayyab47@gmail.com>
2024-04-12 00:33:19 +0500

    rotation added

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,34 +1,46 @@
 import { OrbitControls} from "@react-three/drei";
 import { Interactive, useHitTest, useXR } from "@react-three/xr";
 import { Fragment, useEffect, useRef, useState } from "react";
-import {   useThree } from "@react-three/fiber";
+import {   useFrame, useThree } from "@react-three/fiber";
 import { Experience } from "../Experience";
 import Table from "../3dComponents/Table";
 import {BenchExperience} from "../BenchExperience"
 import { useModelContext } from "../../context/ModelContext";
-// import * as THREE from 'three';
+import * as THREE from 'three';
+
 
 const Gallery = () => {
+
+  
   const reticleRef = useRef();
   const modelRef = useRef();
-
-
-
  
 const [models, setModels] = useState([]);
 const [currentPosition, setCurrentPosition] = useState();
 
 
+
 const {isPresenting} = useXR();
 const {currentModel, setCurrentModel} = useModelContext();
 
-// const [selected, setSelected] = useState();
-// const [scale, setScale] = useState([1,1,1]);
 
-// useEffect(() => {
-//   setSelected(currentModel);
-// }, [currentModel, setCurrentModel]);
 
+const [mouseDown, setMouseDown] = useState(false);
+const [initialX, setInitialX] = useState(false);
+const [finalX, setFinalX] = useState(false);
+
+const [currentStored, setCurrentStored] = useState(false);
+const [current, setCurrent] = useState(null);
+// Function to calculate rotation based on touch or mouse movement
+// Apply rotation to the model
+useFrame(() => {
+  if(mouseDown) {
+    if(modelRef.current && initialX!== current){
+      console.log(initialX, current)
+      modelRef.current.rotation.y += THREE.MathUtils.degToRad(initialX/100);
+    }
+  }
+},[])
 
 useThree(({camera}) => {
   if(!isPresenting){
@@ -55,22 +67,52 @@ const palceModel = (e) => {
   reticleRef.current.visible = false;
 }
 
+const showStart = (e) => {
+  setInitialX(0);
+  if (!currentStored) {
+    setMouseDown(true);
+    const val = parseInt(e.intersection?.point.x * 100000/120);
+    setCurrent(val)
+    setInitialX(val);
+    setCurrentStored(true);
+  } else {
+    setMouseDown(true)
+    const val = parseInt(e.intersection?.point.x * 100000/120);
+    setInitialX(val-current);
+  }
+};
+const showEnd = (e) =>{
+  setMouseDown(false)
+  setInitialX(0)
+  releaseMouse();
+  console.log(initialX)
+}
+
+const releaseMouse = () => {
+  setInitialX(0)
+  setFinalX(0);
+  setCurrentStored(false);
+  setCurrent(0)
+}
+
   return (
     <>
-        <OrbitControls/>
+        {/* <OrbitControls/> */}
         <ambientLight/>
         {isPresenting && 
           models.map(({position, id})=>{
             return (
-              <Fragment key={id}>
-                <mesh ref={modelRef} position={position} scale={[0.5,0.5,0.5]}>
+              <Interactive  onMove={showStart} onSelectEnd={showEnd} onSelectMissed={showEnd}>
+              <Fragment key={id} >
+                <mesh on ref={modelRef} position={position} scale={[0.5,0.5,0.5]}>
                     <Table/>
                 </mesh>
                 </Fragment>
+                </Interactive>
           );
           })
         }
-          
+
         {isPresenting &&
         (
          <Interactive onSelect={palceModel}>
